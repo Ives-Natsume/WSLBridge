875a67958d19718de855b9a20e7cb5827ee897f9
Ives-Natsume <134116579+Ives-Natsume@users.noreply.github.com>
2024-07-08 13:11:55 +0800

    v1.1

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 import tkinter as tk
 from tkinter import scrolledtext
+import sys
+import os
 from lib import *
+
+
+try:
+    import pyi_splash
+    pyi_splash.close()
+except ImportError:
+    pass
+ 
+
+def get_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except AttributeError:
+        base_path = os.path.abspath(".")
+ 
+    return os.path.normpath(os.path.join(base_path, relative_path))
 
 
 # handler
@@ -9,77 +27,105 @@ def on_copy_win_to_linux():
     linuxTargetPath = entry_linux_path.get()
     result = cp_win_to_linux(winFilePath, linuxTargetPath)
     text_result.insert(tk.END, result + "\n")
+    text_result.see(tk.END)
 
 def on_copy_linux_to_win():
     linuxFilePath = entry_linux_file.get()
     winTargetPath = entry_win_path.get()
     result = cp_linux_to_win(linuxFilePath, winTargetPath)
     text_result.insert(tk.END, result + "\n")
+    text_result.see(tk.END)
 
 def on_winPath_to_linuxPath():
     win_path = entry_win_path.get()
     result = winPath_to_linuxPath(win_path)
     text_result.insert(tk.END, result + "\n")
+    text_result.see(tk.END)
 
 def on_mv_win_to_linux():
     winFilePath = entry_win_file.get()
     linuxTargetPath = entry_linux_path.get()
     result = mv_win_to_linux(winFilePath, linuxTargetPath)
     text_result.insert(tk.END, result + "\n")
+    text_result.see(tk.END)
 
 def on_mv_linux_to_win():
     linuxFilePath = entry_linux_file.get()
     winTargetPath = entry_win_path.get()
     result = mv_linux_to_win(linuxFilePath, winTargetPath)
     text_result.insert(tk.END, result + "\n")
+    text_result.see(tk.END)
 
 
 # gui
 root = tk.Tk()
-root.title("W&L Bridge")
+root.title("WSLBridge")
+
+root.iconbitmap(get_path("app_icon.ico"))
 
 
 
 # input zone
 input_zone = tk.Frame(root)
-input_zone.pack()
+input_zone.pack(fill="x", expand=True)
 
 label_win_file = tk.Label(input_zone, text="Windows File Path:")
 label_win_file.pack(anchor= "w")
 
 entry_win_file = tk.Entry(input_zone, width=80)
-entry_win_file.pack(anchor= "w")
+entry_win_file.pack(anchor= "w", fill="x", expand=True)
 
 label_linux_path = tk.Label(input_zone, text="Linux Target Path:")
 label_linux_path.pack(anchor= "w")
 
 entry_linux_path = tk.Entry(input_zone, width=80)
-entry_linux_path.pack(anchor= "w")
+entry_linux_path.pack(anchor= "w", fill="x", expand=True)
 
 label_linux_file = tk.Label(input_zone, text="Linux File Path:")
 label_linux_file.pack(anchor= "w")
 
 entry_linux_file = tk.Entry(input_zone, width=80)
-entry_linux_file.pack(anchor= "w")
+entry_linux_file.pack(anchor= "w", fill="x", expand=True)
 
 label_win_path = tk.Label(input_zone, text="Windows Target Path:")
 label_win_path.pack(anchor= "w")
 
 entry_win_path = tk.Entry(input_zone, width=80)
-entry_win_path.pack(anchor= "w")
+entry_win_path.pack(anchor= "w", fill="x", expand=True)
 
 
 # output zone
-output_zone = tk.Frame(root)
-output_zone.pack(pady= 20)
+output_zone = tk.Frame(root, width=80)
+output_zone.pack(pady= 20, fill="both", expand=True)
 
 
-label_text = tk.Label(output_zone, text="Command Output:")
-label_text.pack(anchor= "w")
+sub_zone = tk.Frame(output_zone)
+sub_zone.pack(fill="x")
+
+label_zone = tk.Frame(sub_zone)
+label_zone.grid(row=0, column=0, sticky="w")
+
+label_text = tk.Label(label_zone, text="Command Output:")
+label_text.pack(side="left")
+
+
+pannel_zone = tk.Frame(sub_zone)
+pannel_zone.grid(row=0, column=1, sticky="e")
+
+def clear_text():
+    text_result.delete(1.0, tk.END)
+
+clear_button = tk.Button(pannel_zone, text="Clear Output", command=clear_text)
+clear_button.pack(side="right")
+
+
+sub_zone.grid_rowconfigure(0, weight=1)
+sub_zone.grid_columnconfigure(0, weight=1)
+sub_zone.grid_columnconfigure(1, weight=1)
+
+
 text_result = scrolledtext.ScrolledText(output_zone, width=78, height=20)
-#text_result["font"] = custom_font
-text_result.pack(anchor= "w")
-
+text_result.pack(fill="both", expand=True)
 
 
 # operate zone
